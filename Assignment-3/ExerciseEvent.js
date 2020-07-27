b2bb2a828c52cbeac2033c44152d4ecf5f8e983d
DebasmitBiswal <debasmitbiswal@outlook.com>
2020-07-27 21:36:05 +0530

    Almost done

--- Assignment-3/ExerciseEvent.js
+++ Assignment-3/ExerciseEvent.js
@@ -1,10 +1,22 @@
 var pwd = document.querySelector('#password');
 var rpwd = document.querySelector('#repassword');
+var usr = document.querySelector('#username');
 var btns = document.querySelector('.btn');
 
+document.ready(function(){
+    if (rpwd.value!="" && usr.value != "" && pwd.value != "") {
+        btns.attr('disabled', false);
+    }
+}
+)
+
+
+
 btns.addEventListener('click',function(event){
-    if (pwd.value != rpwd.value) {
+    if(pwd.value != rpwd.value) {
         alert('Kindly Re-Enter the password');
+    }else if(pwd.value == rpwd.value && usr.value != "" && pwd.value != ""){
+        alert('Congratulation logged in');
     }
 })
 
